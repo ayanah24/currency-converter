610bbab5b98d26ced0584c690b32e0e035680466
ayanah24 <ayan27452@gmail.com>
2025-10-17 23:57:34 +0530

    change 1

--- src/hooks/useCurrencyinfo.js
+++ src/hooks/useCurrencyinfo.js
@@ -2,13 +2,15 @@ import { useEffect, useState } from "react";
 
 function useCurrencyInfo(base = "usd") {
   const [data, setData] = useState({});
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true); 
+// 'loading' is a boolean state used to indicate whether the API call is currently in progress.
+//which prevents blank screen and undefined errors 
 
   useEffect(() => {
     if (!base) return;
     const curr = base.toLowerCase();
 
-    const url = `https://open.er-api.com/v6/latest/${curr}`; // ✅ Reliable API
+    const url = `https://open.er-api.com/v6/latest/${curr}`; 
 
     setLoading(true);
     fetch(url)
